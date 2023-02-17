579cb85742c54006699ddb91ec6157f2bf8297e4
Théo <theonicod@hotmail.Fr>
2023-02-17 11:34:13 +0100

    ignore

--- src/api/quizz.js
+++ src/api/quizz.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const token = sessionStorage.getItem('token')
+//const token = sessionStorage.getItem('token')
 
 export default axios.create({
     baseURL:'http://localhost:3500',
